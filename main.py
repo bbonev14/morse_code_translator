440299c984ec4c9e67079a60cb75ba7883ec56f9
Bogdan <118459007+bbonev14@users.noreply.github.com>
2022-12-22 12:56:24 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,35 @@
+MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
+                   'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-',
+                   'L': '.-..', 'M': '--', 'N': '-.',
+                   'O': '---', 'P': '.--.', 'Q': '--.-',
+                   'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--',
+                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                   '1': '.----', '2': '..---', '3': '...--',
+                   '4': '....-', '5': '.....', '6': '-....',
+                   '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                   '?': '..--..', '/': '-..-.', '-': '-....-',
+                   '(': '-.--.', ')': '-.--.-', ' ':  '/'}
+
+
+def encrypt(message):
+    answer = [MORSE_CODE_DICT[letter] for letter in list(message) if letter in MORSE_CODE_DICT]
+    return ' '.join(answer)
+
+
+def decrypt(morse_code):
+    morse_code = morse_code.split(' ')
+    answer = []
+    for code in morse_code:
+        answer += [letter for letter in MORSE_CODE_DICT if MORSE_CODE_DICT[letter] == code]
+    return ' '.join(answer)
+
+
+string = input('What do you want to translate to morse code?: ').upper()
+x = encrypt(string)
+print(x)
+
+print(decrypt(x))
